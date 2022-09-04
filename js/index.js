5b965338aa99881b71615187ecf3acb3394eb396
riyad5541 <abdullahalriyad5541@gmail.com>
2022-09-04 11:53:50 +0600

    change html

--- js/index.js
+++ js/index.js
@@ -5,6 +5,22 @@ const loadCategories = async () => {
   displayCategories(data.data.news_category)
   // displayCategories (data.data.news_category);
 }
+const displayCategories = allCategories => {
+  // console.log(allCategories)
+  const allCategoriesContainer = document.getElementById('categories-container');
+  for (const categories of allCategories) {
+    // console.log(categories)
+
+    const div = document.createElement('div');
+    div.classList.add('navbar');
+    div.innerHTML = `
+    <a onclick="loadAllCategories('${categories.category_id}')" class="navbar-brand" href="#"">${categories.category_name}</a>
+    `;
+    allCategoriesContainer.appendChild(div);
+  }
+}
+
+
 
 
 
